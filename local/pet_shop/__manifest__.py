812c81638078f0ec5d7810c2dbc6315b082f6ba7
Daniel de Carvalho Soares <dancarsoa@gmail.com>
2025-08-25 13:52:02 -0300

    Criado o módulo Pet Shop

---
+++ local/pet_shop/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Pet Shop',
+    'version': '14.0.1.0',
+    'category': 'Sales',
+    'summary': 'Site Pet Shop',
+    'description': 'Loja para loja de pets',
+    'author': 'Daniel Soares',
+    'maintainer': 'Daniel Soares',
+    'application': True,
+    'installable': True,
+    'license': 'AGPL-3'
+}
+
